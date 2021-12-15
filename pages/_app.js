5f0416cdbed01bc5c3908210dde2e61524ab9716
jauhari.giftano <jauhari@giftano.com>
2021-12-15 13:53:45 +0700

    🏗️ change some architecture

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+      <footer>&copy; testing footer boss</footer>
+    </div>
+  )
 }
 
 export default MyApp
